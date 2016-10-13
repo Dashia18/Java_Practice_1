ac4edbb3e0ef2b334f2646717f4ad16109c7b93c
dbobkova <das-bobkova@yandex.ru>
2016-10-13 14:25:50 +0400

    Class MyPoint already finished without: public String toString()

--- src/MyPoint.java
+++ src/MyPoint.java
@@ -7,13 +7,35 @@ public class MyPoint {
         _y = 0;
     }
     public MyPoint(int x, int y){
+        setXY(x, y);
+    }
+    public int getX(){  return _x;  }
+    public int getY(){  return _y;  }
+    public void setX(int x){    _x = x;   }
+    public void setY(int y){    _y = y;   }
+
+    public int[] getXY() {
+        int[] points = new int[]{_x, _y};
+        return  points;
+    }
+    public void setXY(int x, int y){
         setX(x);
         setY(y);
     }
-    public int getX(){ return _x;  }
-    public int getY(){ return _y;  }
-    public void setX(int x){  _x = x;   }
-    public void setY(int y){  _y = y;   }
+    //public String toString(){ return "0";}
+
+    public double distance(int x, int y){
+        double d = Math.sqrt(Math.pow((_x - x),2) + Math.pow((_y - y),2));
+        return d;
+    }
+    public double distance(MyPoint another){
+        double d = distance(another._x,  another._y);
+        return d;
+    }
+    public double distance(){
+        double d = distance(0, 0);
+        return d;
+    }
 
     private int _x;
     private int _y;
