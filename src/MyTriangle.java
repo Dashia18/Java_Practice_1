b849cdaf3628270d80b69dc199cd1bba3c3a2f17
dbobkova <das-bobkova@yandex.ru>
2016-10-13 15:05:31 +0400

    add two methods to class MyPoint// initialization TBD

--- src/MyTriangle.java
+++ src/MyTriangle.java
@@ -13,6 +13,16 @@ public class MyTriangle {
         return "MyTriangle [v1 = " + _v1 + ", v2 = " + _v2 + ", v3 = " + _v3  + "]";
     }
 
+    public double getPerimeter(){
+        double perimeter = 0;//P = a + b + c
+        return perimeter;
+    }
+    public String getType(){
+        String type = "0";//Equilateral, Isosceles, Scalene
+        return type;
+    }
+
+
     private MyPoint _v1;
     private MyPoint _v2;
     private MyPoint _v3;
