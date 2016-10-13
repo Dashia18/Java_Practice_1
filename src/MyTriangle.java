f5817511058b2fbe24cf3bb3fc93e8e20b642cc1
dbobkova <das-bobkova@yandex.ru>
2016-10-13 14:57:24 +0400

    Init: class MyTriangle - create main structure of the class

---
+++ src/MyTriangle.java
@@ -0,0 +1,19 @@
+/**
+ * Created by dbobkova on 13.10.2016.
+ */
+public class MyTriangle {
+    public MyTriangle(int x1, int y1, int x2, int y2, int x3, int y3){
+
+    }
+    public MyTriangle(MyPoint v1, MyPoint v2, MyPoint v3){
+
+    }
+
+    public String toString(){
+        return "MyTriangle [v1 = " + _v1 + ", v2 = " + _v2 + ", v3 = " + _v3  + "]";
+    }
+
+    private MyPoint _v1;
+    private MyPoint _v2;
+    private MyPoint _v3;
+}
